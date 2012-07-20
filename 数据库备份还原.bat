f2c23d0873cc0bf4a57757a1d9b73b090d921efe
li <li@aihlp.com>
2012-07-20 11:37:00 +0800

    11

--- 数据库备份还原.bat
+++ 数据库备份还原.bat
@@ -12,12 +12,12 @@ if /i "%cmd%"=="r" goto :restore
 goto :input
 
 :backup
-osql -S localhost -U sa -P 123456 -q   "BACKUP  DATABASE [HHU] TO  DISK = N'C:\Desktop\Data\%filename%.bak' WITH NOFORMAT, INIT,  NAME = N'HHU-完整 数据库 备份', SKIP, NOREWIND, NOUNLOAD,  STATS = 10"
+osql -S localhost -U sa -P 123456 -q   "BACKUP  DATABASE [HHU] TO  DISK = N'%filename%' WITH NOFORMAT, INIT,  NAME = N'HHU-完整 数据库 备份', SKIP, NOREWIND, NOUNLOAD,  STATS = 10"
 
 goto :end
 
 :restore
-osql -S localhost -U sa -P 123456 -q   "RESTORE DATABASE [HHU] FROM  DISK = N'C:\Desktop\Data\%filename%.bak' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 10"
+osql -S localhost -U sa -P 123456 -q   "RESTORE DATABASE [HHU] FROM  DISK = N'%filename%' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 10"
 :end
  
 @echo on
