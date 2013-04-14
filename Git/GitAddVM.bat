134290f9ab78ae9cdb13222d5f2cbc90f2cf0c32
li <li@aihlp.com>
2013-04-14 23:17:16 +0800

    11

---
+++ Git/GitAddVM.bat
@@ -0,0 +1,247 @@
+cd bit
+git remote add VM  git@lcj.aihlp.com:bit.git
+cd ..
+cd cepripro
+git remote add VM  git@lcj.aihlp.com:cepripro.git
+cd ..
+cd cepri_release
+git remote add VM  git@lcj.aihlp.com:cepri_release.git
+cd ..
+cd cmd
+git remote add VM  git@lcj.aihlp.com:cmd.git
+cd ..
+cd connect
+git remote add VM  git@lcj.aihlp.com:connect.git
+cd ..
+cd demo
+git remote add VM  git@lcj.aihlp.com:demo.git
+cd ..
+cd dnspodclientlite
+git remote add VM  git@lcj.aihlp.com:dnspodclientlite.git
+cd ..
+cd ebook
+git remote add VM  git@lcj.aihlp.com:ebook.git
+cd ..
+cd ems22xx
+git remote add VM  git@lcj.aihlp.com:ems22xx.git
+cd ..
+cd evc_li
+git remote add VM  git@lcj.aihlp.com:evc_li.git
+cd ..
+cd example
+git remote add VM  git@lcj.aihlp.com:example.git
+cd ..
+cd feeddemon
+git remote add VM  git@lcj.aihlp.com:feeddemon.git
+cd ..
+cd fetion
+git remote add VM  git@lcj.aihlp.com:fetion.git
+cd ..
+cd fss
+git remote add VM  git@lcj.aihlp.com:fss.git
+cd ..
+cd git
+git remote add VM  git@lcj.aihlp.com:git.git
+cd ..
+cd gitolite-admin
+git remote add VM  git@lcj.aihlp.com:gitolite-admin.git
+cd ..
+cd gpgkey
+git remote add VM  git@lcj.aihlp.com:gpgkey.git
+cd ..
+cd hhu2.0
+git remote add VM  git@lcj.aihlp.com:hhu2.0.git
+cd ..
+cd hhumain
+git remote add VM  git@lcj.aihlp.com:hhumain.git
+cd ..
+cd hhuv1.8
+git remote add VM  git@lcj.aihlp.com:hhuv1.8.git
+cd ..
+cd ignore
+git remote add VM  git@lcj.aihlp.com:ignore.git
+cd ..
+cd jboss6.1
+git remote add VM  git@lcj.aihlp.com:jboss6.1.git
+cd ..
+cd k25
+git remote add VM  git@lcj.aihlp.com:k25.git
+cd ..
+cd keymanhub
+git remote add VM  git@lcj.aihlp.com:keymanhub.git
+cd ..
+cd keys
+git remote add VM  git@lcj.aihlp.com:keys.git
+cd ..
+cd km25
+git remote add VM  git@lcj.aihlp.com:km25.git
+cd ..
+cd kmtdoc
+git remote add VM  git@lcj.aihlp.com:kmtdoc.git
+cd ..
+cd lidb
+git remote add VM  git@lcj.aihlp.com:lidb.git
+cd ..
+cd liwin32
+git remote add VM  git@lcj.aihlp.com:liwin32.git
+cd ..
+cd liwince
+git remote add VM  git@lcj.aihlp.com:liwince.git
+cd ..
+cd maxthon
+git remote add VM  git@lcj.aihlp.com:maxthon.git
+cd ..
+cd mercurial
+git remote add VM  git@lcj.aihlp.com:mercurial.git
+cd ..
+cd modetest
+git remote add VM  git@lcj.aihlp.com:modetest.git
+cd ..
+cd onenote
+git remote add VM  git@lcj.aihlp.com:onenote.git
+cd ..
+cd othercompanydemo
+git remote add VM  git@lcj.aihlp.com:othercompanydemo.git
+cd ..
+cd pgpcom
+git remote add VM  git@lcj.aihlp.com:pgpcom.git
+cd ..
+cd pgp
+git remote add VM  git@lcj.aihlp.com:pgp.git
+cd ..
+cd pgpsdk
+git remote add VM  git@lcj.aihlp.com:pgpsdk.git
+cd ..
+cd photo
+git remote add VM  git@lcj.aihlp.com:photo.git
+cd ..
+cd s8600
+git remote add VM  git@lcj.aihlp.com:s8600.git
+cd ..
+cd sec
+git remote add VM  git@lcj.aihlp.com:sec.git
+cd ..
+cd site
+git remote add VM  git@lcj.aihlp.com:site.git
+cd ..
+cd svndb
+git remote add VM  git@lcj.aihlp.com:svndb.git
+cd ..
+cd svn
+git remote add VM  git@lcj.aihlp.com:svn.git
+cd ..
+cd svntogit
+git remote add VM  git@lcj.aihlp.com:svntogit.git
+cd ..
+cd testing
+git remote add VM  git@lcj.aihlp.com:testing.git
+cd ..
+cd warcraft
+git remote add VM  git@lcj.aihlp.com:warcraft.git
+cd ..
+cd xian
+git remote add VM  git@lcj.aihlp.com:xian.git
+cd ..
+cd xiaomi
+git remote add VM  git@lcj.aihlp.com:xiaomi.git
+cd ..
+cd apk
+git remote add VM  git@lcj.aihlp.com:apk.git
+cd ..
+cd tools
+git remote add VM  git@lcj.aihlp.com:li/tools.git
+cd ..
+cd concentrator
+git remote add VM  git@lcj.aihlp.com:li/concentrator.git
+cd ..
+cd connector2
+git remote add VM  git@lcj.aihlp.com:li/connector2.git
+cd ..
+cd donet
+git remote add VM  git@lcj.aihlp.com:li/donet.git
+cd ..
+cd expressdelivery
+git remote add VM  git@lcj.aihlp.com:li/expressdelivery.git
+cd ..
+cd firefox
+git remote add VM  git@lcj.aihlp.com:li/firefox.git
+cd ..
+cd foxmail
+git remote add VM  git@lcj.aihlp.com:li/foxmail.git
+cd ..
+cd fss2doc
+git remote add VM  git@lcj.aihlp.com:li/fss2doc.git
+cd ..
+cd hhu1.8release
+git remote add VM  git@lcj.aihlp.com:li/hhu1.8release.git
+cd ..
+cd hhui
+git remote add VM  git@lcj.aihlp.com:li/hhui.git
+cd ..
+cd hhumain2
+git remote add VM  git@lcj.aihlp.com:li/hhumain2.git
+cd ..
+cd hhumainYJ
+git remote add VM  git@lcj.aihlp.com:li/hhumainYJ.git
+cd ..
+cd hhutype
+git remote add VM  git@lcj.aihlp.com:li/hhutype.git
+cd ..
+cd k36
+git remote add VM  git@lcj.aihlp.com:li/k36.git
+cd ..
+cd keygen
+git remote add VM  git@lcj.aihlp.com:li/keygen.git
+cd ..
+cd lib
+git remote add VM  git@lcj.aihlp.com:li/lib.git
+cd ..
+cd li
+git remote add VM  git@lcj.aihlp.com:li/li.git
+cd ..
+cd livs2012
+git remote add VM  git@lcj.aihlp.com:li/livs2012.git
+cd ..
+cd md_release
+git remote add VM  git@lcj.aihlp.com:li/md_release.git
+cd ..
+cd msd
+git remote add VM  git@lcj.aihlp.com:li/msd.git
+cd ..
+cd p31
+git remote add VM  git@lcj.aihlp.com:li/p31.git
+cd ..
+cd protocol
+git remote add VM  git@lcj.aihlp.com:li/protocol.git
+cd ..
+cd proxy
+git remote add VM  git@lcj.aihlp.com:li/proxy.git
+cd ..
+cd rapi
+git remote add VM  git@lcj.aihlp.com:li/rapi.git
+cd ..
+cd redminekmt
+git remote add VM  git@lcj.aihlp.com:li/redminekmt.git
+cd ..
+cd release_henan
+git remote add VM  git@lcj.aihlp.com:li/release_henan.git
+cd ..
+cd report
+git remote add VM  git@lcj.aihlp.com:li/report.git
+cd ..
+cd shunzhou
+git remote add VM  git@lcj.aihlp.com:li/shunzhou.git
+cd ..
+cd study
+git remote add VM  git@lcj.aihlp.com:li/study.git
+cd ..
+cd tp900
+git remote add VM  git@lcj.aihlp.com:li/tp900.git
+cd ..
+cd vmloader
+git remote add VM  git@lcj.aihlp.com:li/vmloader.git
+cd ..
+cd chrome
+git remote add VM  git@lcj.aihlp.com:li/chrome.git
+cd ..
+pause
