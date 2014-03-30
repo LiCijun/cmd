e87666b1dfab59bf6ab078c75262a1458daf57da
li <li@aihlp.com>
2014-03-30 15:15:18 +0800

    更新库

---
+++ fssdb.bat
@@ -0,0 +1,36 @@
+@echo off  
+  
+:: BatchGotAdmin  
+:-------------------------------------  
+REM  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
+  
+REM --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (  
+    echo Requesting administrative privileges...  
+    goto UACPrompt  
+) else ( goto gotAdmin )  
+  
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
+  
+    "%temp%\getadmin.vbs"  
+    exit /B  
+  
+:gotAdmin  
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
+    pushd "%CD%"  
+    CD /D "%~dp0"  
+:--------------------------------------  
+@echo off
+echo Start FSS
+net start OracleMTSRecoveryService
+net start OracleServiceFSS
+net start OracleOraDb11g_home1TNSListener
+echo Will Stop FSS
+pause
+net stop OracleOraDb11g_home1TNSListener
+net stop OracleServiceFSS
+net stop OracleMTSRecoveryService
+pause
---
+++ lidb.bat
@@ -0,0 +1,38 @@
+@echo off  
+  
+:: BatchGotAdmin  
+:-------------------------------------  
+REM  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
+  
+REM --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (  
+    echo Requesting administrative privileges...  
+    goto UACPrompt  
+) else ( goto gotAdmin )  
+  
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
+  
+    "%temp%\getadmin.vbs"  
+    exit /B  
+  
+:gotAdmin  
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
+    pushd "%CD%"  
+    CD /D "%~dp0"  
+:--------------------------------------  
+@echo off
+echo Start li
+net start OracleMTSRecoveryService
+net start OracleServiceLI
+net start OracleOraDb11g_home1TNSListener
+net start OracleDBConsoleli
+echo Will Stop li
+pause
+net stop OracleDBConsoleli
+net stop OracleOraDb11g_home1TNSListener
+net stop OracleServiceLI
+net stop OracleMTSRecoveryService
+pause
