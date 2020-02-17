a5b0cd4a4d1038eae3d97553f5ee31a22eab9fc8
li <lcj211@aihlp.com>
2020-02-17 10:44:10 +0800

    OK

---
+++ scpenv.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -e
+if [ ! -n "$1" ] ;
+then   
+echo "请传入远程主机" ;
+exit
+fi
+host=$1
+scp ~/.bashrc $host:~/
+scp ~/.nanorc $host:~/
+scp ~/.gitconfig $host:~/
+scp ~/.config/fish/config.fish $host:~/.config/fish/
+
+#rsync -avP  ~/.config/fish/config.fish  $1:~/.config/fish/
+
+
+
+
+
+
+#scp /etc/sudoers  $host:/etc/
+
+
+
+
+
+
