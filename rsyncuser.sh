50c9b8cdfbe67f32dd16c8759c1abec4a1c2de83
li <lcj211@aihlp.com>
2020-03-17 12:28:54 +0800

    OK

--- rsyncuser.sh
+++ rsyncuser.sh
@@ -6,15 +6,11 @@ echo "请传入远程主机" ;
 exit
 fi
 host=$1
-#rsync -av ~/.bash_login $host:~/
-rsync -avhP ~/.bashrc $host:~/
-ssh $host echo  Start user at $host 
-rsync -avhP ~/.profile $host:~/
-rsync -avhP ~/.bash_profile $host:~/
-rsync -avhP ~/.nanorc $host:~/
-rsync -avhP ~/.gitconfig $host:~/
-rsync -avhP ~/.xprofile $host:~/
-rsync -avhP ~/.config/fish/  $host:~/.config/fish/
-
-
-
+#rsync -av ~/.bash_login $host:~/ no file
+rsync -ah ~/.bashrc $host:~/
+rsync -ah ~/.profile $host:~/
+rsync -ah ~/.bash_profile $host:~/
+rsync -ah ~/.nanorc $host:~/
+rsync -ah ~/.gitconfig $host:~/
+rsync -ah ~/.xprofile $host:~/
+rsync -ah ~/.config/fish/  $host:~/.config/fish/
