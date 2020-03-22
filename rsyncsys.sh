330d42cc13deabb6114dc14a0bdc17ad697d0f7a
li <lcj211@aihlp.com>
2020-03-22 10:54:33 +0800

    OK

--- rsyncsys.sh
+++ rsyncsys.sh
@@ -12,7 +12,7 @@ rsync -ahAX /etc/environment $host:/etc/
 rsync -ahAX /etc/profile $host:/etc/
 rsync -ahAX /etc/ssh/ssh*config  $host:/etc/ssh/
 rsync -ahAX /etc/pacman.conf  $host:/etc/
-rsync -ahAX /etc/skel/.config/locale.conf   $host:/etc/skel/.config/
+sudo rsync -ahAX /etc/skel/.config/locale.conf   $host:/etc/skel/.config/
 sudo rsync -ahAX /etc/pacman.d/   $host:/etc/pacman.d/
 sudo rsync -ahAX /etc/sudoers  $host:/etc/
 
