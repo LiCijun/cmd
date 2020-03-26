edbae88547815fd87118ae613c7258c81e7427a4
li <lcj211@aihlp.com>
2020-03-26 16:43:08 +0800

    Ok

--- rsyncos.sh
+++ rsyncos.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 set -e
 echo 操作系统备份
+$1=nas:/share/osbak/
 if [ ! -n "$1" ] ;
 then   
 echo "请传入备份路径" ;
 exit
 fi
 host=$1
-host=/home/li/nasDir/osbak/
+
 
 if [ ! -n "$2" ] ;
 then   
-sudo rsync -ahAXvP --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home",".snapshots","/home/li/nasDir/osbak",$host}  --exclude-from=rsyncexcludefile                         /  $host
+sudo rsync -ahAXvP --delete -e 'ssh' --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home",".snapshots","/home/li/nasDir/osbak",$host}  --exclude-from=rsyncexcludefile                         /  $host
 else
 oldPath=$2
-sudo rsync -ahAXvP --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home",".snapshots","/home/li/nasDir/osbak",$host}  --exclude-from=rsyncexcludefile   --link-dest=$oldPath  /  $host
+sudo rsync -ahAXvP --delete -e  'ssh' --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home",".snapshots","/home/li/nasDir/osbak",$host}  --exclude-from=rsyncexcludefile   --link-dest=$oldPath  /  $host
 fi
 
 
