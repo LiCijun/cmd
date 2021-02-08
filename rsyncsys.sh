460734d2fdddfa51ceb3394f13985036650d35cd
li <lcj211@aihlp.com>
2021-02-08 09:37:04 +0800

    修改：     rsyncsys.sh

--- rsyncsys.sh
+++ rsyncsys.sh
@@ -6,17 +6,20 @@ echo "请传入远程主机" ;
 exit
 fi
 host=$1
-sudo rsync -ahAX   /etc/conf.d/reflector.conf 							$host:/etc/conf.d/
-rsync -ahAX  /etc/locale.conf 										$host:/etc/
-rsync -ahAX /etc/security/limits.conf   								$host:/etc/security/
-rsync -ahAX /etc/environment 											$host:/etc/
-rsync -ahAX /etc/profile $host:/etc/
-rsync -ahAX /etc/ssh/ssh*config  $host:/etc/ssh/
-rsync -ahAX /etc/pacman.conf  $host:/etc/
-rsync -ahAX  /etc/pacman.d/archlinuxcn-mirrorlist  $host:/etc/pacman.d/
-sudo rsync -ahAX /etc/skel/.config/locale.conf   $host:/etc/skel/.config/
-sudo rsync -ahAX /etc/pacman.d/   $host:/etc/pacman.d/
-sudo rsync -ahAX /etc/sudoers  $host:/etc/
+sudo rsync -ahPAXv   /etc/conf.d/reflector.conf 							$host:/etc/conf.d/
+rsync -ahPAXv  /etc/locale.conf 										$host:/etc/
+rsync -ahPAXv /etc/security/limits.conf   								$host:/etc/security/
+rsync -ahPAXv /etc/environment 											$host:/etc/
+rsync -ahPAXv /etc/profile $host:/etc/
+rsync -ahPAXv /etc/ssh/ssh*config  $host:/etc/ssh/
+rsync -ahPAXv /etc/pacman.conf  $host:/etc/
+rsync -ahPAXv  /etc/pacman.d/archlinuxcn-mirrorlist  $host:/etc/pacman.d/
+sudo rsync -ahPAXv /etc/skel/.config/locale.conf   $host:/etc/skel/.config/
+sudo rsync -ahPAXv /etc/pacman.d/   $host:/etc/pacman.d/
+sudo rsync -ahPAXv /etc/sudoers  $host:/etc/
+sudo rsync -ahPAXv /etc/xdg/reflector/  $host:/etc/xdg/reflector/
+
+
 
 
 
